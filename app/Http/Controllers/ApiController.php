a7b9fa8d55964b56bc58757fc123b3ed42cf5fef
PontiiPilates <s.m.leshukov@gmail.com>
2022-10-14 12:47:54 +0700

    Refactoring API-system more

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,7 +8,7 @@ use App\Models\Car;
 use App\Models\Driver;
 use App\Models\Driving;
 
-class DrivingController extends Controller
+class ApiController extends Controller
 {
 
     /**
@@ -19,20 +19,22 @@ class DrivingController extends Controller
      * @return \Illuminate\Http\JsonResponse
      */
 
-    public function drivingCreate(Request $r)
+    public function create(Request $r)
     {
+
+        /**
+         * Validation code in processed ...
+         */
 
         // попытка получения модели автомобиля: иначе 404
         $car = Car::findOrFail($r->car_id);
+        // попытка получения моедли водителя: иначе 404
+        $driver = Driver::findOrFail($r->driver_id);
 
         // проверка статуса автомобиля: 0 - свободен / 1 - занят
         if ($car->status === 1) {
             return response()->json(['message' => 'Автомобиль уже используется'], 200);
         }
-
-        // попытка получения моедли водителя: иначе 404
-        $driver = Driver::findOrFail($r->driver_id);
-
         // проверка статуса водителя: 0 - не управляет / 1 - управляет
         if ($driver->status === 1) {
             return response()->json(['message' => 'Водитель уже управляет автомобилем'], 200);
@@ -43,12 +45,11 @@ class DrivingController extends Controller
 
         // обновление статуса автомобиля: занят
         $car->update(['status' => 1]);
-
         // обновление статуса водителя: управляет
         $driver->update(['status' => 1]);
 
         // возвращает идентификатор поездки
-        return response()->json(['driving_number' => $driving->id], 201);
+        return response()->json(['driving_id' => $driving->id], 201);
 
     }
 
@@ -60,29 +61,30 @@ class DrivingController extends Controller
      * @return \Illuminate\Http\JsonResponse
      */
 
-    public function drivingComplete($driving_id)
+    public function complete($driving_id)
     {
+
+        /**
+         * Validation code in processed ...
+         */
 
         // попытка получения модели поездки: иначе 404
         $driving = Driving::findOrFail($driving_id);
-
-        // получение модели автомобиля, который участвует в поездке
-        $car = Car::find($driving->car_id);
-
-        // получение модели водителя, который участвует в поездке
-        $driver = Driver::find($driving->driver_id);
 
         // проверка статуса поездки: 0 - продолжается / 1 - завершена
         if ($driving->status === 1) {
             return response()->json(['message' => 'Поездка больше недоступна для изменения'], 200);
         }
 
+        // получение модели автомобиля, который участвует в поездке
+        $car = Car::find($driving->car_id);
+        // получение модели водителя, который участвует в поездке
+        $driver = Driver::find($driving->driver_id);
+
         // обновление статуса автомобиля: свободен
         $car->update(['status' => 0]);
-
         // обновление статуса водителя: не управляет
         $driver->update(['status' => 0]);
-
         // обновление статуса поездки: завершена
         $driving->update(['status' => 1]);
 
